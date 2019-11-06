45caf82959bae4374f9470d7d05c57f066386447
rahul <snakepowerpoint@gmail.com>
2019-11-06 16:28:49 +0800

    incorporate pre-trained VGG19

--- model.py
+++ model.py
@@ -37,26 +37,30 @@ def encoder(inputs, reuse=False):
 def decoder(encode):
     with tf.variable_scope("decoder"):
         # up sampling 1
-        x = tf.layers.conv2d(encode, filters=256, kernel_size=(3, 3), padding='same', activation='relu')
-        x = tf.layers.conv2d(x, filters=256, kernel_size=(3, 3), padding='same', activation='relu')
-        x = tf.layers.conv2d(x, filters=128, kernel_size=(3, 3), padding='same', activation='relu')
-        x = tf.layers.conv2d(x, filters=128, kernel_size=(3, 3), padding='same', activation='relu')
+        x = tf.layers.conv2d(encode, filters=256, kernel_size=(2, 2), padding='same', activation='relu')
         shape = tf.shape(x)
         h, w = shape[1], shape[2]
         x = tf.image.resize_nearest_neighbor(x, tf.stack([h*2, w*2]))
         
         # up sampling 2
-        x = tf.layers.conv2d(x, filters=64, kernel_size=(3, 3), padding='same', activation='relu')
+        x = tf.layers.conv2d(x, filters=256, kernel_size=(3, 3), padding='same', activation='relu')
+        x = tf.layers.conv2d(x, filters=256, kernel_size=(3, 3), padding='same', activation='relu')
+        x = tf.layers.conv2d(x, filters=256, kernel_size=(3, 3), padding='same', activation='relu')
+        x = tf.layers.conv2d(x, filters=128, kernel_size=(3, 3), padding='same', activation='relu')
+        shape = tf.shape(x)
+        h, w = shape[1], shape[2]
+        x = tf.image.resize_nearest_neighbor(x, tf.stack([h*2, w*2]))
+
+        # up sampling 3
+        x = tf.layers.conv2d(x, filters=128, kernel_size=(3, 3), padding='same', activation='relu')
         x = tf.layers.conv2d(x, filters=64, kernel_size=(3, 3), padding='same', activation='relu')
         shape = tf.shape(x)
         h, w = shape[1], shape[2]
         x = tf.image.resize_nearest_neighbor(x, tf.stack([h*2, w*2]))
 
-        # output
+        # up sampling 4 (output)
+        x = tf.layers.conv2d(x, filters=64, kernel_size=(3, 3), padding='same', activation='relu')
         x = tf.layers.conv2d(x, filters=3, kernel_size=(3, 3), padding='same', activation='relu')
-        shape = tf.shape(x)
-        h, w = shape[1], shape[2]
-        x = tf.image.resize_nearest_neighbor(x, tf.stack([h*2, w*2]))
 
     return x
 
@@ -68,182 +72,21 @@ def adain(content_encode, style_encode, epsilon=1e-5):
 
     return s_std * (content_encode - c_mean) / c_std + s_mean
 
-def conv4(inputs, reuse=False, training=True):
-    with tf.variable_scope("conv4", reuse=reuse):
-        # block 1
-        x = conv_block(inputs=inputs, out_channels=64, ksize=3, block=1, training=training)
-        x = spatial_attention(feature_map=x, out_channels=64, block=1)
-
-        # block 2
-        x = conv_block(x, 64, 3, 2, training)
-        x = spatial_attention(feature_map=x, out_channels=64, block=2)
-
-        # block 3
-        x = conv_block(x, 64, 3, 3, training)
-        x = spatial_attention(feature_map=x, out_channels=64, block=3)
-
-        # block 4
-        x = conv_block(x, 64, 3, 4, training)
-        x = spatial_attention(feature_map=x, out_channels=64, block=4)
-
-        # global average pooling
-        avg_pool = tf.reduce_mean(x, axis=[1, 2], name='avg_pool', keepdims=True)
-        flatten = tf.layers.flatten(avg_pool)
-
-    return flatten
-
-def conv_block(inputs, out_channels, ksize, block, training):
-    block_name = 'block' + str(block)
-    with tf.variable_scope(block_name):
-        x = tf.layers.conv2d(inputs, out_channels, ksize, padding='same')
-        x = tf.layers.batch_normalization(x, axis=-1, training=training)
-        x = tf.nn.relu(x)
-        x = tf.layers.max_pooling2d(inputs=x, pool_size=[2, 2], strides=2)
-
-    return x
-
-def spatial_attention(feature_map, out_channels, block):
-    block_name = 'block' + str(block) + '_spa_att'
-    with tf.variable_scope(block_name):
-        # down sampling 1
-        x = tf.layers.conv2d(inputs=feature_map, filters=out_channels, kernel_size=3, padding='same')
-        x = tf.layers.max_pooling2d(inputs=x, pool_size=[2, 2], strides=2, padding='same')
-        short_cut = tf.layers.conv2d(inputs=x, filters=out_channels, kernel_size=3, padding='same')
-
-        # down sampling 2
-        x = tf.layers.conv2d(inputs=x, filters=out_channels, kernel_size=3, padding='same')
-        x = tf.layers.max_pooling2d(inputs=x, pool_size=[2, 2], strides=2, padding='same')
-        print(x.get_shape())
-
-        # up sampling 1
-        x = tf.layers.conv2d(inputs=x, filters=out_channels, kernel_size=3, padding='same')
-        print(x.get_shape())
-        x = tf.keras.layers.UpSampling2D(size=[2, 2])(x)
-        print(short_cut.get_shape())
-        x += short_cut
-
-        # up sampling 2
-        x = tf.layers.conv2d(inputs=x, filters=out_channels, kernel_size=3, padding='same')
-        x = tf.keras.layers.UpSampling2D(size=[2, 2])(x)
-        
-        # residual attention
-        x = (1 + feature_map) * x
-
-    return x
-
-def resnet12(inputs, training):  # num_class
-    with tf.variable_scope("resnet"):
-        # block 1
-        x = convolutional_block(inputs, kernel_size=3, in_filter=3, out_filter=64, block=1, training=training)
-
-        # block 2
-        x = convolutional_block(inputs, 3, 64, 96, 2, training)
-
-        # block 3
-        x = convolutional_block(inputs, 3, 96, 128, 3, training)
-
-        # block 4
-        x = convolutional_block(inputs, 3, 128, 256, 4, training)
-    
-        # global average pooling
-        w_conv1 = weight_variable(shape=[1, 1, 256, 2048])
-        x = tf.nn.conv2d(x, w_conv1, strides=[1, 1, 1, 1], padding='SAME')
-
-        avg_pool = tf.nn.avg_pool(x, ksize=[1, 6, 6, 1], strides=[1, 1, 1, 1], padding='VALID')
-        avg_pool = tf.nn.relu(avg_pool)
-        avg_pool = tf.nn.dropout(avg_pool, keep_prob=0.9)
-
-        w_conv2 = weight_variable(shape=[1, 1, 2048, 384])
-        x = tf.nn.conv2d(x, w_conv2, strides=[1, 1, 1, 1], padding='SAME')
-
-        avg_pool = tf.reduce_mean(x, axis=[1, 2], name='avg_pool', keepdims=True)
-        flatten = tf.layers.flatten(avg_pool)
-
-        # fc layer
-        #fc1 = tf.layers.dense(flatten, units=512, activation=tf.nn.relu)
-        #fc2 = tf.layers.dense(flatten, units=num_class, activation=tf.nn.softmax)
-        
-    return flatten
-        
-def convolutional_block(inputs, kernel_size, in_filter, out_filter, block, training):
-    block_name = 'block' + str(block)
-    with tf.variable_scope(block_name):
-        short_cut = inputs
-
-        # first
-        w_conv1 = weight_variable(shape=[kernel_size, kernel_size, in_filter, out_filter])
-        x = tf.nn.conv2d(inputs, w_conv1, strides=[1, 1, 1, 1], padding='SAME')
-        x = tf.layers.batch_normalization(x, axis=-1, training=training)
-        x = tf.nn.leaky_relu(x, alpha=0.1)
-
-        # second
-        w_conv2 = weight_variable(shape=[kernel_size, kernel_size, out_filter, out_filter])
-        x = tf.nn.conv2d(x, w_conv2, strides=[1, 1, 1, 1], padding='SAME')
-        x = tf.layers.batch_normalization(x, axis=-1, training=training)
-        x = tf.nn.leaky_relu(x, alpha=0.1)
-
-        # third
-        w_conv3 = weight_variable(shape=[kernel_size, kernel_size, out_filter, out_filter])
-        x = tf.nn.conv2d(x, w_conv3, strides=[1, 1, 1, 1], padding='SAME')
-        x = tf.layers.batch_normalization(x, axis=-1, training=training)
-        x = tf.nn.leaky_relu(x, alpha=0.1)
-
-        # shortcut
-        w_short = weight_variable(shape=[1, 1, in_filter, out_filter])
-        short_cut = tf.nn.conv2d(short_cut, w_short, strides=[1, 1, 1, 1], padding='SAME')
-
-        # add short cut
-        add = tf.add(x, short_cut)
-        add = tf.nn.max_pool(add, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-        add = tf.nn.dropout(add, keep_prob=0.9)
-
-    return add
-
-def identity_block(inputs, kernel_size, out_filter, in_filter, stage, block, training):
-    f1, f2 = out_filter
-    block_name = 'res_conv' + str(stage) + "_" + str(block)
-    with tf.variable_scope(block_name):
-        short_cut = inputs
-
-        # first
-        w_conv1 = weight_variable(shape=[kernel_size, kernel_size, in_filter, f1])
-        x = tf.nn.conv2d(inputs, w_conv1, strides=[1, 1, 1, 1], padding='SAME')
-        x = tf.layers.batch_normalization(x, axis=-1, training=training)
-        x = tf.nn.relu(x)
-
-        # second
-        w_conv2 = weight_variable(shape=[kernel_size, kernel_size, f1, f2])
-        x = tf.nn.conv2d(x, w_conv2, strides=[1, 1, 1, 1], padding='SAME')
-        x = tf.layers.batch_normalization(x, axis=-1, training=training)
-
-        # add short cut
-        add = tf.add(x, short_cut)
-        add = tf.nn.relu(add)
-
-    return add
-
-def weight_variable(shape):
-    """weight_variable generates a weight variable of a given shape."""
-    initializer = tf.contrib.layers.xavier_initializer()
-    return tf.Variable(shape=shape, initializer=initializer)
-
 
 
 class VGG19(object):
-    def __init__(self, w_path, input_w, input_h):
+    def __init__(self, w_path):
         self.w_path = w_path
-        self.input_h = input_h
-        self.input_w = input_w
-        self.model = self.vggnet()
-
-    def vggnet(self):
+        
+    def vggnet(self, inputs):
         # load pre-trained vgg-19
         vgg = loadmat(self.w_path)
         vgg_layers = vgg['layers'][0]
         net = {}
 
         # keep cnn and pooling layer
-        net['input'] = tf.Variable(np.zeros([1, self.input_h, self.input_w, 3]), dtype=tf.float32)
+        # tf.Variable(np.zeros([1, self.input_h, self.input_w, 3]), dtype=tf.float32)
+        net['input'] = inputs
         net['conv1_1'] = self.conv_relu(net['input'], self.get_wb(vgg_layers, 0))
         net['conv1_2'] = self.conv_relu(net['conv1_1'], self.get_wb(vgg_layers, 2))
         net['pool1'] = self.pool(net['conv1_2'])
@@ -259,10 +102,10 @@ class VGG19(object):
         net['pool3'] = self.pool(net['conv3_4'])
         
         net['conv4_1'] = self.conv_relu(net['pool3'], self.get_wb(vgg_layers, 19))
-        net['conv4_2'] = self.conv_relu(net['conv4_1'], self.get_wb(vgg_layers, 21))
-        net['conv4_3'] = self.conv_relu(net['conv4_2'], self.get_wb(vgg_layers, 23))
-        net['conv4_4'] = self.conv_relu(net['conv4_3'], self.get_wb(vgg_layers, 25))
-        net['pool4'] = self.pool(net['conv4_4'])
+        # net['conv4_2'] = self.conv_relu(net['conv4_1'], self.get_wb(vgg_layers, 21))
+        # net['conv4_3'] = self.conv_relu(net['conv4_2'], self.get_wb(vgg_layers, 23))
+        # net['conv4_4'] = self.conv_relu(net['conv4_3'], self.get_wb(vgg_layers, 25))
+        # net['pool4'] = self.pool(net['conv4_4'])
         
         # net['conv5_1'] = self.conv_relu(net['pool4'], self.get_wb(vgg_layers, 28))
         # net['conv5_2'] = self.conv_relu(net['conv5_1'], self.get_wb(vgg_layers, 30))
@@ -272,16 +115,13 @@ class VGG19(object):
         
         return net
 
-
     def conv_relu(self, inputs, wb):
         conv = tf.nn.conv2d(inputs, wb[0], strides=[1, 1, 1, 1], padding='SAME')
         relu = tf.nn.relu(conv + wb[1])
         return relu
 
-
     def pool(self, inputs):
         return tf.nn.max_pool(inputs, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-
 
     def get_wb(self, layers, i):
         # load weights (set as constant) from pre-trained vgg model
--- train.py
+++ train.py
@@ -1,104 +1,22 @@
 # tensorflow
 import tensorflow as tf
-from tensorflow.contrib.layers import xavier_initializer
 import tensorflow.contrib.slim as slim
 
 # numpy
 import numpy as np
 
+# io
+import pickle as pkl
+
 # customerized 
-from model import encoder, decoder, adain
+from model import decoder, adain, VGG19
 from losses import compute_content_loss, compute_style_loss
 from utils import resize_image, generate_few_shot_style
 
-import pickle as pkl
-import cv2
+# miscellaneous
+import gc
 
 
-# hyper-parameters
-N_WAY=5
-K_SHOT=1
-K_QUERY=15
-EPISODE=30000
-
-def compute_accuracy():
-    return None
-
-def compute_loss(labels, outputs):
-    with tf.name_scope('loss'):
-        cross_entropy = tf.losses.softmax_cross_entropy(onehot_labels=labels, logits=outputs)
-    return cross_entropy
-
-def generate_prototype(fc1_support, support_labels):
-    num_class = support_labels.shape[1]
-
-    mean_vector = []
-    for i_class in range(num_class):
-        i_class_mask = tf.equal(support_labels[:, i_class], 1)
-        fc1_i_class = tf.boolean_mask(fc1_support, i_class_mask)
-        mean_vector.append(tf.reduce_mean(fc1_i_class, axis=0))
-    mean_vector = tf.stack(mean_vector)
-
-    return mean_vector
-
-def compute_intra_loss(prototype, fc1_query, query_labels):
-    '''
-    Compute intra-class loss
-
-    Args
-        prototype: [5, 512]
-        fc1_query: [75, 512]
-        query_labels: [75, 5]
-        
-    Return
-        scalar
-    '''
-    num_class = prototype.shape[0]
-
-    loss_per_class = []
-    for i_class in range(num_class):
-        i_class_prototype = prototype[i_class]  # i-th prototype
-
-        i_class_mask = tf.equal(query_labels[:, i_class], 1)
-        fc1_i_class = tf.boolean_mask(fc1_query, i_class_mask)  # all queries for i-th class
-
-        i_class_query_norm = tf.norm(fc1_i_class - i_class_prototype, axis=1)
-        loss_per_class.append(tf.reduce_sum(i_class_query_norm))        
-        
-    intra_class_loss = tf.reduce_sum(loss_per_class)
-
-    return intra_class_loss
-
-def compute_inter_loss(prototype, fc1_query, query_labels):
-    '''
-    Compute inter-class loss
-
-    Args
-        prototype: [5, 512]
-        fc1_query: [75, 512]
-        query_labels: [75, 5]
-
-    Return
-        scalar
-    '''
-    num_class = prototype.shape[0]
-
-    loss_per_class = []
-    for i_class in range(num_class):
-        i_class_prototype = prototype[i_class]  # i-th prototype
-
-        # select all queries that are not i-class
-        i_class_mask = tf.equal(query_labels[:, i_class], 0)
-        fc1_not_i_class = tf.boolean_mask(fc1_query, i_class_mask)  
-
-        i_class_other_query_norm = tf.norm(fc1_not_i_class - i_class_prototype, axis=1)
-        i_class_loss = tf.exp(tf.negative(i_class_other_query_norm))  # exp(-d(prototype, y))
-        i_class_loss = tf.log(tf.reduce_sum(i_class_loss))  # log(sum(loss_per_class))
-        loss_per_class.append(i_class_loss)
-
-    inter_class_loss = tf.reduce_sum(loss_per_class)
-
-    return inter_class_loss
 
 def compute_distance(fc1_query, prototype):
     '''
@@ -123,7 +41,6 @@ def compute_distance(fc1_query, prototype):
 def compute_acc(prediction, one_hot_labels):
     labels = tf.argmax(one_hot_labels, axis=1)
     acc = tf.reduce_mean(tf.to_float(tf.equal(tf.argmax(prediction, axis=-1), labels)))
-
     return acc
 
 def model_summary():
@@ -135,58 +52,78 @@ def main():
     # load MNIST and MNIST-M
     (m_train, m_train_y), (m_test, m_test_y) = tf.keras.datasets.mnist.load_data()
     mm = pkl.load(open('data/mnistm_data.pkl', 'rb'))
-    mm_train, mm_train_y = mm['train'], mm['train_label']
+    mm_train, mm_train_y = mm['train'], mm['train_label']; del mm
 
-    # #  keep numbers 0-4 in MNIST as content, and numbers 5-9 in MNIST-M as style
-    # content_image = m_train[m_train_y < 5, ...]
-    # content_image_y = m_train_y[m_train_y < 5]
-    # content_image = resize_image(content_image, size=(32, 32))
-    # content_image = np.repeat(content_image[..., np.newaxis], 3, axis=-1)
-    # test_content_image = m_train[m_train_y >= 5, ...]
-    
-    # style_image = mm_train[mm_train_y >= 5, ...]
-    # style_image_y = mm_train[mm_train_y >= 5]
-    # style_image, style_image_y = generate_few_shot_style(style_image, style_image_y, num_sample=5)
-    # style_image = resize_image(style_image, size=(32, 32))
-
-    # use all train data in MNIST as content image, and all train data in MNIST-M as style image
-    content_image = resize_image(m_train, size=(32, 32))
+    #  keep numbers 0-4 in MNIST as content, and numbers 5-9 in MNIST-M as style
+    content_image = m_train[m_train_y < 5, ...]
+    content_image_y = m_train_y[m_train_y < 5]
     content_image = np.repeat(content_image[..., np.newaxis], 3, axis=-1)
-    test_content_image = m_test
-
-    style_image = resize_image(mm_train, size=(32, 32))
-
     
+    test_content_image = m_train[m_train_y >= 5, ...]; del m_train
+    test_content_image_y = m_train_y[m_train_y >= 5]; del m_train_y
+    test_content_image = np.repeat(test_content_image[..., np.newaxis], 3, axis=-1)
+    
+    style_image = mm_train[mm_train_y >= 5, ...]; del mm_train
+    style_image_y = mm_train_y[mm_train_y >= 5]; del mm_train_y
+    style_image, style_image_y = generate_few_shot_style(style_image, style_image_y, num_sample=5)
+    
+    # resize
+    content_image = resize_image(content_image, size=(32, 32))
+    test_content_image = resize_image(test_content_image, size=(32, 32))
+    style_image = resize_image(style_image, size=(32, 32))    
+
+    # # use all train data in MNIST as content image, and all train data in MNIST-M as style image
+    # content_image = np.repeat(m_train[..., np.newaxis], 3, axis=-1); del m_train
+    
+    # test_content_image = np.repeat(m_test[..., np.newaxis], 3, axis=-1); del m_test
+    
+    # # resize    
+    # content_image = resize_image(content_image, size=(32, 32))
+    # test_content_image = resize_image(test_content_image, size=(32, 32))
+    # style_image = resize_image(mm_train, size=(32, 32)); del mm_train
+    
+    # normalize
+    content_image = content_image / 255
+    style_image = style_image / 255
+    test_content_image = test_content_image / 255
+
+    gc.collect()
+
+
     ## prepare model
+    model_path = "model/imagenet-vgg-verydeep-19.mat"
+    encoder = VGG19(model_path)
+
     # inputs placeholder
     c_img = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
     s_img = tf.placeholder(tf.float32, shape=[None, 32, 32, 3])
     
     # establish model
-    c_encode, _ = encoder(c_img)
-    s_encode, s_layers = encoder(s_img, reuse=True)
+    c_encode = encoder.vggnet(c_img)
+    s_encode = encoder.vggnet(s_img)
 
-    c_adain_encode = adain(c_encode, s_encode)
+    c_adain_encode = adain(c_encode['conv4_1'], s_encode['conv4_1'])
     styled_img = decoder(c_adain_encode)
-    styled_encode, styled_layers = encoder(styled_img, reuse=True)
+    styled_encode = encoder.vggnet(styled_img)
 
     # loss
-    content_loss = compute_content_loss(styled_encode, c_adain_encode)
-    style_loss = compute_style_loss(styled_layers, s_layers)
+    content_loss = compute_content_loss(styled_encode['conv4_1'], c_adain_encode)
+    style_loss = compute_style_loss(styled_encode, s_encode)
     total_loss = content_loss + 0.01 * style_loss
-
+    
     # optimizer 
-    optimizer = tf.train.AdamOptimizer(1e-4)
+    optimizer = tf.train.AdamOptimizer(1e-3)
     train_op = optimizer.minimize(total_loss)
     
     model_summary()
+
 
     ## training
     init = tf.global_variables_initializer()
     with tf.Session() as sess:
         # Creates a file writer for the log directory.
-        logdir = "logs/"
-        file_writer = tf.summary.FileWriter(logdir, sess.graph)
+        file_writer_train = tf.summary.FileWriter("logs/train_l1e3/", sess.graph)
+        file_writer_test = tf.summary.FileWriter("logs/test_l1e3/", sess.graph)
 
         # store variables
         tf.summary.image("Content image", c_img, max_outputs=10)
@@ -201,10 +138,12 @@ def main():
 
         # total number of data
         num_data = content_image.shape[0]
+
         batch_size = 8
         num_batch = num_data // batch_size
+        num_episode = 3000
 
-        for i_episode in range(EPISODE):
+        for i_episode in range(num_episode):
             # shuffle data
             np.random.shuffle(content_image)
             np.random.shuffle(style_image)
@@ -212,13 +151,11 @@ def main():
             for i_batch in range(num_batch):
                 # get a batch of content
                 c_image = content_image[i_batch*batch_size: (i_batch+1)*batch_size, ...]
-                c_image = c_image / 255
-
+                
                 # random sample a batch of style
                 idx = np.random.choice(style_image.shape[0], batch_size, replace=False)
                 s_image = style_image[idx, ...]
-                s_image = s_image / 255
-
+                
                 # training                 
                 _, train_loss = sess.run([train_op, total_loss], feed_dict={
                     c_img: c_image,
@@ -230,45 +167,29 @@ def main():
                     np.random.shuffle(test_content_image)
                     
                     test_c_image = test_content_image[:10, ...]
-                    test_c_image = resize_image(test_c_image, size=(32, 32))
-                    test_c_image = np.repeat(test_c_image[..., np.newaxis], 3, axis=-1)
-                    test_c_image = test_c_image / 255
+                    test_s_image = style_image[:10, ...]
 
-                    test_s_image = style_image[:10, ...] / 255
+                    summary_train, train_loss = sess.run([merged, total_loss], feed_dict={
+                        c_img: c_image,
+                        s_img: s_image
+                    })
 
-                    summary, test_loss = sess.run([merged, total_loss], feed_dict={
+                    summary_test, test_loss = sess.run([merged, total_loss], feed_dict={
                         c_img: test_c_image,
                         s_img: test_s_image
                     })
 
                     # log all variables
-                    #num_iter = i_episode * num_batch + i_batch
-                    file_writer.add_summary(summary, global_step=i_episode * num_batch + i_batch)
+                    num_iter = i_episode * num_batch + i_batch
+                    file_writer_train.add_summary(summary_train, global_step=num_iter)
+                    file_writer_test.add_summary(summary_test, global_step=num_iter)
 
                     print('Episode: %d, batch: %d, training cost: %g, test cost: %g' %
                           (i_episode, i_batch, train_loss, test_loss))
 
-                    
-        file_writer.close()
+        file_writer_train.close()
+        file_writer_test.close()
 
-            # if i_episode % 100 == 0:
-            #     # evaluation on test data
-            #     content_image = m_train[m_train_y < 5, ...]
-            #     content_image_y = m_train_y[m_train_y < 5]
-            #     content_image = resize_image(content_image, size=(32, 32))
-            #     content_image = np.expand_dims(content_image, axis=-1)
-
-            #     num_test = len(test_data)
-            #     test_losses = []
-            #     for idx in range(num_test):
-            #         test_c_img, test_s_img = test_data[idx]
-            #         test_loss = sess.run([total_loss], feed_dict={
-            #             c_img: test_c_img,
-            #             s_img: test_s_img
-            #         })
-            #         test_losses.append(test_loss)
-
-            #     print('Test cost: %g' % (np.mean(test_losses)))
 
 
 if __name__ == '__main__':
